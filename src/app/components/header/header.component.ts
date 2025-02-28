bb0164393dc90d21dcff8a8a9856014302e60681
zuhyunlee <zuhyunlee@outlook.de>
2025-02-28 14:05:30 +0100

    adjust(header): dynamic layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuCollapsed = true;
 
+  toggleNavbar() {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
 }
